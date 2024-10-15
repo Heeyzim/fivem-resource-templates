6a8c3d88683c148afa63e1a640a21c75e839df2a
Luigi Zampieri <zampieri.luigi17@gmail.com>
2024-10-15 01:20:06 -0300

    updated templates

--- typescript-vanilla/res/client/build.js
+++ typescript-vanilla/res/client/build.js
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import esbuild from "esbuild";
 import pkg from "esbuild-plugin-fileloc";
 const { filelocPlugin } = pkg;
@@ -12,7 +13,10 @@ const buildOptions = (isWatch, platform, entry, outFile) => ({
     ? {
         onRebuild(error) {
           if (error) console.error("Rebuild failed:", error);
-          else console.log("Rebuilt successfully");
+          else {
+            console.log("Rebuilt successfully");
+            generateTypes();
+          }
         },
       }
     : false,
@@ -23,18 +27,25 @@ const buildOptions = (isWatch, platform, entry, outFile) => ({
   plugins: platform === "node" ? [filelocPlugin()] : [],
 });
 
+const generateTypes = () => {
+  exec("tsc --emitDeclarationOnly", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error generating types: ${stderr}`);
+    } else {
+      console.log("Types generated successfully");
+    }
+  });
+};
+
 esbuild
   .build(
-    buildOptions(
-      isWatchMode,
-      "browser",
-      "./src/index.ts",
-      "./dist/client/index.js",
-    ),
+    buildOptions(isWatchMode, "browser", "./src/index.ts", "./dist/index.js"),
   )
-  .then(() =>
-    console.log(
-      `Built client files${isWatchMode ? " (watching for changes)" : ""}`,
-    ),
+  .then(
+    () =>
+      console.log(
+        `Built client files${isWatchMode ? " (watching for changes)" : ""}`,
+      ),
+    generateTypes(),
   )
   .catch(() => process.exit(1));
--- typescript-vanilla/res/server/build.js
+++ typescript-vanilla/res/server/build.js
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import esbuild from "esbuild";
 import pkg from "esbuild-plugin-fileloc";
 const { filelocPlugin } = pkg;
@@ -11,7 +12,10 @@ const buildOptions = (isWatch, platform, entry, outFile) => ({
     ? {
         onRebuild(error) {
           if (error) console.error("Rebuild failed:", error);
-          else console.log("Rebuilt successfully");
+          else {
+            console.log("Rebuilt successfully");
+            generateTypes();
+          }
         },
       }
     : false,
@@ -22,18 +26,23 @@ const buildOptions = (isWatch, platform, entry, outFile) => ({
   plugins: platform === "node" ? [filelocPlugin()] : [],
 });
 
+const generateTypes = () => {
+  exec("tsc --emitDeclarationOnly", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error generating types: ${stderr}`);
+    } else {
+      console.log("Types generated successfully");
+    }
+  });
+};
+
 esbuild
-  .build(
-    buildOptions(
-      isWatchMode,
-      "node",
-      "./src/index.ts",
-      "./dist/server/index.js",
-    ),
-  )
-  .then(() =>
-    console.log(
-      `Built server files${isWatchMode ? " (watching for changes)" : ""}`,
-    ),
+  .build(buildOptions(isWatchMode, "node", "./src/index.ts", "./dist/index.js"))
+  .then(
+    () =>
+      console.log(
+        `Built server files${isWatchMode ? " (watching for changes)" : ""}`,
+      ),
+    generateTypes(),
   )
   .catch(() => process.exit(1));
